ecefb234334ab599dab33067d0c14717e5d0faf0
spnball <surapun.prasit@gmail.com>
2016-01-25 07:37:05 +0700

    Implement mysqlDB

--- mysqlDB/db-importer.sh
+++ mysqlDB/db-importer.sh
@@ -9,10 +9,13 @@ fi
 
 sleep 1
 
+MYSQL_HOST=127.0.0.1
+MYSQL_USER=root
+
 # Waiting for until mysql is started
 while true
 do
-    UP=$(mysql --host="127.0.0.1" --user="root" --password="$MYSQL_ROOT_PASSWORD" \
+    UP=$(mysql --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_ROOT_PASSWORD" \
     -e "show databases;" 2>/dev/null | grep information_schema | wc -l
 );
     if [ "$UP" -ne 0 ];
@@ -30,7 +33,7 @@ echo `date +%Y-%m-%d:%H:%M:%S` "Start initialize mysql data."
 import_database () {
     DATABASE=$1;
     echo `date +%Y-%m-%d:%H:%M:%S` "Checking '$DATABASE' database."
-    DB_EXISTS=$(mysql --host="127.0.0.1" --user="root" --password="$MYSQL_ROOT_PASSWORD"  \
+    DB_EXISTS=$(mysql --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_ROOT_PASSWORD"  \
                 -e "show databases;" 2>/dev/null | grep "^$DATABASE$" | wc -l);
     if [ "$DB_EXISTS" -ne 0 ];
     then
@@ -38,9 +41,11 @@ import_database () {
     else
         echo `date +%Y-%m-%d:%H:%M:%S` "Prepare importing $DATABASE";
         
-        sftp -o "StrictHostKeyChecking no" -i /root/.ssh/id_rsa "$DEV_SSH_HOST":/tmp/acommerce.sql.tar.gz /tmp
+        sftp -o "StrictHostKeyChecking no" -i /root/.ssh/id_rsa "$DEV_SSH_HOST":/tmp/${DATABASE}.sql.tar.gz /root
         
-        tar -xzf /tmp/acommerce.sql.tar.gz /tmp/
+        tar -xzf /root/${DATABASE}.sql.tar.gz
+        mysql --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_ROOT_PASSWORD" < ${DATABASE}.sql
+        rm ${DATABASE}.sql;
     fi
 }
 
