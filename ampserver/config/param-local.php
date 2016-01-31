02ae584ebe1649642efe5b96144d4e85fd8d50ff
spnball <surapun.prasit@gmail.com>
2016-01-31 07:51:16 +0700

    Implement amp server

---
+++ ampserver/config/param-local.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * params-local.php
+ *
+ * @author: antonio ramirez <antonio@clevertech.biz>
+ * Date: 7/22/12
+ * Time: 5:59 PM
+ *
+ *
+ * ANY CONFIGURATION OPTIONS HERE WILL REPLACE THOSE INCLUDED ON THE params-env.php file!!!
+ * It holds your local configuration settings.
+ *
+ * Replace following tokens for your local correspondent configuration data
+ *
+ * {DATABASE-NAME} ->   database name
+ * {DATABASE-HOST} -> database server host name or ip address
+ * {DATABASE-USERNAME} -> user name access
+ * {DATABASE-PASSWORD} -> user password
+ *
+ * {DATABASE-TEST-NAME} ->   Test database name
+ * {DATABASE-TEST-HOST} -> Test database server host name or ip address
+ * {DATABASE-USERNAME} -> Test user name access
+ * {DATABASE-PASSWORD} -> Test user password
+ */
+return array(
+	'env.code' => 'private',
+	
+	// DB connection configurations
+	'db.name' => '',
+	'db.connectionString' => 'mysql:host=database;dbname=acommerce',
+	'db.username' => 'root',
+	'db.password' => 'badboy',
+	
+	// partner database {
+	'partner_db.name' => '',
+	'partner_db.connectionString' => 'mysql:host={DATABASE-HOST};dbname={DATABASE-NAME}',
+	'partner_db.username' => '{DATABASE-USERNAME}',
+	'partner_db.password' => '{DATABASE-PASSWORD}',
+
+	// shipment database {
+	'shipment_db.name' => '',
+	'shipment_db.connectionString' => 'mysql:host=database;dbname=acommerce_shipment',
+	'shipment_db.username' => 'root',
+	'shipment_db.password' => 'badboy',
+
+	'adelivery_db.name' => '',
+	'adelivery_db.connectionString' => 'mysql:host=database;dbname=acommerce_shipment',
+	'adelivery_db.username' => 'root',
+	'adelivery_db.password' => 'badboy',
+
+	'netsuite_db.name' => '',
+	'netsuite_db.connectionString' => 'mysql:host=database;dbname=magento_netsuite',
+	'netsuite_db.username' => 'root',
+	'netsuite_db.password' => 'badboy',
+
+	'external_tracking_db.name' => '',
+	'external_tracking_db.connectionString' => 'mysql:host=database;dbname=acommerce_external_tracking',
+	'external_tracking_db.username' => 'root',
+	'external_tracking_db.password' => 'badboy',
+
+	// delete order database
+	'delete_db.name' => '',
+	'delete_db.connectionString' => 'mysql:host=database;dbname=acommerce_delete',
+	'delete_db.username' => 'root',
+	'delete_db.password' => 'badboy',
+//
+//	// test database {
+//	'testdb.name' => '',
+//	'testdb.connectionString' => 'mysql:host={DATABASE-HOST};dbname={DATABASE-NAME}_test',
+//	'testdb.username' => '{DATABASE-USERNAME}',
+//	'testdb.password' => '{DATABASE-PASSWORD}',
+
+);
---
+++ ampserver/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+AMP_DIR=/var/www/amp
+
+PHP_ERROR_REPORTING=${PHP_ERROR_REPORTING:-"E_ALL & ~E_DEPRECATED & ~E_NOTICE"}
+sed -ri "s/^error_reporting\s*=.*$//g" /etc/php5/apache2/php.ini
+sed -ri "s/^error_reporting\s*=.*$//g" /etc/php5/cli/php.ini
+echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php5/apache2/php.ini
+echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php5/cli/php.ini
+
+mkdir -p /etc/apache2/conf
+cp /root/apache_common.conf /etc/apache2/conf/common.conf
+
+# Create www structure
+mkdir -p /var/log/apache2
+mkdir -p /var/www/default
+cp /root/info.php /var/www/default/
+
+if [ ! -d "/var/www/amp" ]; then
+    ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+    git clone git@bitbucket.org:acommerceplatform/amp-project.git ${AMP_DIR}
+fi
+
+cp /root/main-local.php ${AMP_DIR}/common/config/main-local.php
+cp /root/main-local.php ${AMP_DIR}/common/config/param-local.php
+
+mkdir -p ${AMP_DIR}/console/runtime ${AMP_DIR}/backend/runtime ${AMP_DIR}/frontend/runtime
+chown ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} ${AMP_DIR}/console/runtime ${AMP_DIR}/backend/runtime ${AMP_DIR}/frontend/runtime
+
+mkdir -p ${AMP_DIR}/backend/www/assets
+chown ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} ${AMP_DIR}/backend/www/assets
+
+exec /usr/sbin/apache2 -DFOREGROUND
